2709f98a0794ee04cd95da356979050b40996a6b
Gianluca Vigo <gianluca.vigo.gv@gmail.com>
2025-03-15 15:28:49 +0100

    Added multassign.c and multassignERR.c for testing

---
+++ tests/multassign/multassign.c
@@ -0,0 +1,17 @@
+int d, a, b, c, e, f, g;
+d = 34;
+a, b, c = 10, 23, 41;
+e, f, g = a*2, 16/4, b*c;
+
+// Simple assignment
+write(d); // 34
+
+// Multiple assignment
+write(a); // 10
+write(b); // 23
+write(c); // 41
+
+// Multiple assignment (complex case)
+write(e); // 20
+write(f); // 4
+write(g); // 943
---
+++ tests/multassign/multassignERR.c
@@ -0,0 +1,25 @@
+// More vars than exprs
+int a, b, c;
+
+a, b, c = 10, 13;
+
+
+// More exprs than vars
+/*
+int a, b, c;
+c = 10;
+a, b = 12, 13, 14;
+*/
+
+
+// Array case
+/*
+int a[2], b;
+
+a[1] = 4;
+b, a[0] = 1, 2;
+
+write(a[0]);
+write(a[1]);
+write(b);
+*/
